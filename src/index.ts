03e710e49829917a931e6d2a8a8f12e839ab48e1
bibekthapa007 <bibekthapa922@gmail.com>
2024-11-19 10:35:06 +0545

    Add route to analysis feedback

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { analyzeFeedbacks } from "./utils/csvHandler";
+import { analyzeFeedback } from "./utils/analyzeFeedback";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get("/summarize", async (req: Request, res: Response) => {
   try {
@@ -18,6 +21,29 @@ app.get("/summarize", async (req: Request, res: Response) => {
     res
       .status(200)
       .json({ message: "Feedback analysis complete!", outputFilePath });
+  } catch (error) {
+    console.error("Error analyzing feedback:", error);
+    res.status(500).json({ message: "Error analyzing feedback", error });
+  }
+});
+
+app.post("/feedback", async (req: Request, res: Response) => {
+  try {
+    const feedback = req.body?.feedback;
+    console.log(req.body);
+
+    if (!feedback) {
+      console.log("Feedback is required.");
+      res.status(500).json({ message: "Feedback is required" });
+    }
+
+    const analayzedFeedback = await analyzeFeedback(feedback);
+
+    res.status(200).json({
+      message: "Feedback analysis complete!",
+      feedback,
+      analayzedFeedback,
+    });
   } catch (error) {
     console.error("Error analyzing feedback:", error);
     res.status(500).json({ message: "Error analyzing feedback", error });
